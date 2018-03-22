6e444d43eddca137fd24bf54c4cad13eaf582e38
Chanderpaul Martin <chanderpaul.martin@mymona.uwi.edu>
2018-03-22 00:30:08 +0000

    final

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://xsywcgiromherd:a0bf8466d3f88e12a27bfeba16e6496e3c248b92e8a7ab7a48c8953a1d60eb27@ec2-23-21-217-27.compute-1.amazonaws.com:5432/dcqreahlb0hupu"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
@@ -13,9 +13,7 @@ db = SQLAlchemy(app)
 UPLOAD_FOLDER ='./app/static/uploads'
 
 # Flask-Login login manager
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+
 
 app.config.from_object(__name__)
 from app import views
